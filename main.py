567a0f74cc4c19809c7e0134b848197395978e77
Raphael Reme <raphael.reme@telecom-paristech.fr>
2021-11-30 16:31:15 +0100

    Use Hand rather than holistic solution
    + Fix some bugs

--- main.py
+++ main.py
@@ -3,40 +3,43 @@ import mediapipe
 import pandas as pd
 from utils import *
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     sample_list = []
     num_samples = 1000
-    count = 0
     recording = False
 
     blue_color = (245, 25, 16)
     red_color = (24, 44, 245)
     color = blue_color
 
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    with mediapipe.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    cap = cv2.VideoCapture(0)
+
+    with mediapipe.solutions.hands.Hands(max_num_hands=1, min_detection_confidence=0.7) as hands:
         while cap.isOpened():
             # Read feed
             ret, frame = cap.read()
 
             # Make detections
-            frame, results = mediapipe_detection(frame, holistic)
+            frame, results = mediapipe_detection(frame, hands)
+
+            landmarks = results.multi_hand_landmarks
 
             # Draw landmarks
-            draw_landmarks(frame, results)
+            draw_landmarks(frame, landmarks)
 
-            if recording and count < num_samples:
-                sample_name = "sample_" + ("000" + str(count))[:-4]
-                sample = mp_landmarks_to_np_array(results.right_hand).reshape(63).tolist()
-                sample_list.append([sample_name] + sample)
+            if recording and len(sample_list) < num_samples:
+                sample_name = f"sample_{len(sample_list):05d}"
+                if landmarks:
+                    sample = mp_landmarks_to_np_array(landmarks[0]).reshape(63).tolist()
+                    sample_list.append([sample_name] + sample)
 
-                count += 1
                 color = red_color
+                print(f"Recording: {len(sample_list)}/{num_samples}", end="\r")
 
-            elif recording and count == num_samples:
-                pd.DataFrame(sample_list).to_csv("hand_gesture.csv", sep=',')
-                count = 0
+            elif recording and len(sample_list) == num_samples:
+                pd.DataFrame(sample_list).to_csv("hand_gesture.csv", sep=",")
+
                 recording = False
                 sample_list = []
                 color = blue_color
@@ -45,14 +48,15 @@ if __name__ == '__main__':
             cv2.circle(frame, (30, 30), 20, color, -1)
 
             # Show to screen
-            cv2.imshow('OpenCV Feed', frame)
+            frame = cv2.flip(frame, 1)
+            cv2.imshow("OpenCV Feed", frame)
 
             # Break pressing q
-            if cv2.waitKey(5) & 0xFF == ord('q'):
+            if cv2.waitKey(5) == ord("q"):
                 break
 
             # Record pressing s
-            if cv2.waitKey(5) & 0xFF == ord('s'):
+            if cv2.waitKey(5) == ord("s"):
                 recording = True
 
         cap.release()
--- utils.py
+++ utils.py
@@ -4,33 +4,29 @@ import numpy as np
 
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR CONVERSION RGB 2 BGR
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # Image is no longer writeable
+    results = model.process(image)  # Make prediction
+    image.flags.writeable = True  # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR CONVERSION RGB 2 BGR
     return image, results
 
 
-def draw_landmarks(image, results):
-    mp_holistic = mp.solutions.holistic  # Holistic model
+def draw_landmarks(image, landmarks):
+    if not landmarks:
+        return
+
+    mp_hands = mp.solutions.hands  # Holistic model
     mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(235, 52, 86), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(52, 235, 103), thickness=2, circle_radius=2)
-                              )
-    # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(235, 52, 86), thickness=1, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(52, 55, 235), thickness=2, circle_radius=2)
-                              )
-    # Draw right hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(235, 52, 86), thickness=1, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(52, 55, 201), thickness=2, circle_radius=2)
-                              )
+    # Draw hands
+    mp_drawing.draw_landmarks(
+        image,
+        landmarks[0],
+        mp_hands.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(235, 52, 86), thickness=1, circle_radius=4),
+        mp_drawing.DrawingSpec(color=(52, 55, 235), thickness=2, circle_radius=2),
+    )
 
 
 def mp_landmarks_to_np_array(mp_landmarks):
